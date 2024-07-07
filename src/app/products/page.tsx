f028ace3770b2418d26236a1b3b2b82b3cb833f9
Abdullah Salman <abdullah.salman@devsinc.com>
2024-07-07 12:49:39 +0500

    dynamic nested routing added

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <>
+      <h1>Products</h1>
+      <h4>p1</h4>
+      <h4>p2</h4>
+      <h4>p3</h4>
+    </>
+  );
+};
+
+export default Products;
