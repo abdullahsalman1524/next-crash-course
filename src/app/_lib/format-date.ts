b291b43982b5e5baeab00bf5b7daab566b379b24
Abdullah Salman <abdullah.salman@devsinc.com>
2024-07-07 13:29:48 +0500

    private folders

---
+++ src/app/_lib/format-date.ts
@@ -0,0 +1,6 @@
+// if _ is added before folder name it is  private (not available for routing );
+
+
+// usefull in seprating route files with logic files
+// for sorting and grouping files in routers
+// consistenly organizing internal files across the project.
